a67e1c3f36ff21abd34cc22767cc80e454bdf18a
Heylga <oy.golovina@gmail.com>
2022-01-24 10:33:16 +0000

    const are declared

--- src/app.js
+++ src/app.js
@@ -1,12 +1,26 @@
-function validateForm() {
-  let x = document.forms["myForm"]["fname"].value;
-  if (x == "") {
-    alert("Name must be filled out");
-    return false;
-  }
-}
+const nameinput = document.querySelectorAll("#name");
+const password = document.querySelectorAll("#password");
+const form = document.querySelectorAll("#form");
+const errorElement = document.querySelectorAll("#error");
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = function(validateform) {
+  form.addEventListner("submit", e => {
+    let messages = [];
+    if (nameinput.value === "" || nameinput.value == null) {
+      messages.push("Name is requiered");
+    }
+
+    if (password.value === "" || password.value == null) {
+      messages.push("Password is requiered");
+    }
+
+    // if (password.value.length <= 6) {
+    //   messages.push("Password must be longer than 6 characters");
+    // }
+
+    if (messages.length > 0) {
+      errorElement.prevetDefault();
+      errorElement.innerText = messages.join(", ");
+    }
+  });
 };
